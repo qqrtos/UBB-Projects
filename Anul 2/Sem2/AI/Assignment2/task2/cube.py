4b901303b0d22745fc015225b7fbe1ebfa4a6f38
Dragos Cirtorosan <56576401+qqrtos@users.noreply.github.com>
2021-03-14 22:45:58 +0200

    A.I. greedy drone search implementation.

--- Anul 2/Sem2/AI/Assignment2/task2/cube.py
+++ Anul 2/Sem2/AI/Assignment2/task2/cube.py
@@ -16,65 +16,61 @@ verticies = (
     (1, 1, 1),
     (-1, -1, 1),
     (-1, 1, 1)
-    )
+)
 
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
 
 surfaces = (
-    (0,1,2,3),
-    (3,2,7,6),
-    (6,7,5,4),
-    (4,5,1,0),
-    (1,5,7,2),
-    (4,0,3,6)
-    )
-
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
 
 colors = (
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (0,1,0),
-    (1,1,1),
-    (0,1,1),
-    (1,0,0),
-    (0,1,0),
-    (0,0,1),
-    (1,0,0),
-    (1,1,1),
-    (0,1,1),
-    )
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (0, 1, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+    (1, 0, 0),
+    (0, 1, 0),
+    (0, 0, 1),
+    (1, 0, 0),
+    (1, 1, 1),
+    (0, 1, 1),
+)
 
 
 def Cube():
     glBegin(GL_QUADS)
-    
+
     for surface in surfaces:
         x = 0
 
         for vertex in surface:
-            x+=1
+            x += 1
             glColor3fv(colors[x])
             glVertex3fv(verticies[vertex])
-        
+
     glEnd()
-    
 
-
-    
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
@@ -84,25 +80,19 @@ def Cube():
 
 def main():
     pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
-    
-    
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
-
-    #start further back
-    glTranslatef(random.randrange(-5,5),0, -30)
-
+    # start further back
+    glTranslatef(random.randrange(-5, 5), 0, -30)
 
     # no more rotate
-    #glRotatef(25, 2, 1, 0)
-
+    # glRotatef(25, 2, 1, 0)
 
     object_passed = False
-    
+
     ind = False
 
     while not object_passed:
@@ -114,14 +104,14 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    glTranslatef(-0.5,0,0)
+                    glTranslatef(-0.5, 0, 0)
                 if event.key == pygame.K_RIGHT:
-                    glTranslatef(0.5,0,0)
+                    glTranslatef(0.5, 0, 0)
 
                 if event.key == pygame.K_UP:
-                    glTranslatef(0,1,0)
+                    glTranslatef(0, 1, 0)
                 if event.key == pygame.K_DOWN:
-                    glTranslatef(0,-1,0)
+                    glTranslatef(0, -1, 0)
             '''
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4:
@@ -133,32 +123,27 @@ def main():
         if ind:
             break
 
-        
-        x = glGetDoublev(GL_MODELVIEW_MATRIX)#, modelviewMatrix)
+        x = glGetDoublev(GL_MODELVIEW_MATRIX)  # , modelviewMatrix)
 
         camera_x = x[3][0]
         camera_y = x[3][1]
         camera_z = x[3][2]
 
-        #print(camera_x,camera_y,camera_z)
-        
+        # print(camera_x,camera_y,camera_z)
 
         # slowly move:
-        glTranslatef(0,0,0.5)
+        glTranslatef(0, 0, 0.5)
 
-
-        
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
-
 
         if camera_z <= 0:
             object_passed = True
         pygame.time.delay(250)
     pygame.quit()
     sys.exit()
-            
 
-for x in range(100):
+
+for _ in range(100):
     main()
