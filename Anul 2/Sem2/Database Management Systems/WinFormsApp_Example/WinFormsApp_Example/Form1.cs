fa8028c390a483ff453a0b32f0f2ff1b1bcd686e
Dragos Cirtorosan <56576401+qqrtos@users.noreply.github.com>
2021-03-21 19:54:10 +0200

    Databases WinForms - DB connection example.

---
+++ Anul 2/Sem2/Database Management Systems/WinFormsApp_Example/WinFormsApp_Example/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormsApp_Example {
+    public partial class Form1 : Form {
+        private SqlConnection connection;
+        private DataSet dataSet = new DataSet();
+        private SqlDataAdapter shipsAdaptor, piratesAdaptor;
+        private SqlCommandBuilder commandBuilder;
+        private BindingSource shipsSource = new BindingSource(), piratesSource = new BindingSource();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            connection = new SqlConnection("Data Source=laptop-a2lrj4vg;Initial Catalog=Pirates;Integrated Security=True");
+
+            shipsAdaptor = new SqlDataAdapter("select * from ships", connection);
+            piratesAdaptor = new SqlDataAdapter("select * from pirates", connection);
+
+            commandBuilder = new SqlCommandBuilder(shipsAdaptor);
+
+            shipsAdaptor.Fill(dataSet, "ships");
+            piratesAdaptor.Fill(dataSet, "pirates");
+
+            DataRelation relation = new DataRelation("FK_Pirates_Ships", dataSet.Tables["ships"].Columns["ShipID"], dataSet.Tables["pirates"].Columns["ShipID"]);
+            dataSet.Relations.Add(relation);
+
+            shipsSource.DataSource = dataSet;
+            shipsSource.DataMember = "ships";
+
+            piratesSource.DataSource = dataSet;
+            piratesSource.DataMember = "FK_Pirates_Ships";
+
+            shipsGridView.DataSource = shipsSource;
+            piratesGridView.DataSource = piratesSource;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e) {
+
+        }
+       
+    }
+}
