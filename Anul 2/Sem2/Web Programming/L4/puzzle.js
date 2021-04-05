9ad6d7107cb1500a3be0099c43ad8de4aae04868
Dragos Cirtorosan <56576401+qqrtos@users.noreply.github.com>
2021-04-05 20:46:36 +0300

    Vanilla JS 8 Tiles Puzzle

--- Anul 2/Sem2/Web Programming/L4/puzzle.js
+++ Anul 2/Sem2/Web Programming/L4/puzzle.js
@@ -1,19 +1,57 @@
-var TILES_IDS = ["A", "B", "C", "D", "E", "F", "G", "H"]
-var COMPLETE_STATUS = [
+var TILES_IDS = ["A", "B", "C", "D", "E", "F", "G", "H", "empty"]
+var STATUS = [
     ["A", "B", "C"],
     ["D", "E", "F"],
-    ["G", "H", ""]
+    ["G", "empty", "H"]
 ]
 
 function isGameFinished(){
     return TILES_IDS
         .map(id => document.getElementById(id))
-        .filter(e => e.id != e.textContent)
+        .filter(e => e.id != e.textContent && e.id !== "empty")
         .length == 0;
 }
 
+function moveToEmptyTile(target){
+    var position = target.getAttribute("pos")
+                         .split(',')
+                         .map(a => parseInt(a));
+
+    var x = position[0];
+    var y = position[1];
+
+    function updateTiles(targetX, targetY){
+        let targetTile = document.querySelectorAll(`[pos="${targetX},${targetY}"]`)[0];
+        targetTile.classList.remove("empty");
+        targetTile.innerHTML = STATUS[x][y];
+        document.querySelectorAll(`[pos="${x},${y}"]`)[0].classList.add("empty");
+
+        let aux = STATUS[x][y];
+        STATUS[x][y] = STATUS[targetX][targetY];
+        STATUS[targetX][targetY] = aux;
+    }
+
+    if(x - 1 >= 0 && STATUS[x - 1][y] === "empty") {
+        updateTiles(x - 1, y);
+    }
+    else if(x + 1 < 3 && STATUS[x + 1][y] === "empty") {
+        updateTiles(x + 1, y);
+    }
+    else if(y - 1 >= 0 && STATUS[x][y - 1] === "empty") {
+        updateTiles(x, y - 1);
+    }
+    else if(y + 1 > 0 && STATUS[x][y + 1] === "empty") {
+        updateTiles(x, y + 1);
+    }
+}
+
 function handleTileClick(target) {
-    console.log(target.id);
+    if (isGameFinished()) {
+        alert("WINNER WINNER!");
+        return;
+    }
+    
+    moveToEmptyTile(target);
 }
 
 (() => {
